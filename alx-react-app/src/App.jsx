164e7a7642234c099c2bde229492c00d577333aa
David-Abia <abiadavid534@gmail.com>
2026-02-07 14:15:28 +0100

    fixed bug

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,3 @@
-import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -6,16 +5,11 @@ import Footer from './Footer';
 function App() {
   return (
     <>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
 
 export default App;
-
-
-
