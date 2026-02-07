280484b446dae55f276331cd75ecc45265e901aa
David-Abia <abiadavid534@gmail.com>
2026-02-07 13:59:46 +0100

    added a userpofile file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -5,11 +6,16 @@ import Footer from './Footer';
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </>
   );
 }
 
 export default App;
+
+
+
