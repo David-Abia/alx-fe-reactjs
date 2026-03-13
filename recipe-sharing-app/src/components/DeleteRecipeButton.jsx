af24e75e1f64fd070c4c605591e9384ede7fe781
David-Abia <abiadavid534@gmail.com>
2026-03-13 19:26:39 +0100

    added useNavigate function

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,16 @@
 import { useRecipeStore } from "../store/recipeStore";
+import { useNavigate } from "react-router-dom";
 
 function DeleteRecipeButton({ recipeId }) {
 
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
+  const navigate = useNavigate();
+
   const handleDelete = () => {
     deleteRecipe(recipeId);
+
+    navigate("/");
   };
 
   return (
