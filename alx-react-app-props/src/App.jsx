3e67f3919a10df63cf072de24b5e2b65573bc305
David-Abia <abiadavid534@gmail.com>
2026-03-13 13:21:35 +0100

    added userProfile to src

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,6 @@ import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
 
 function App() {
-
   const userData = {
     name: "Jane Doe",
     email: "jane.doe@example.com"
